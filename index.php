5c376753b6875292f2408c32a1316a05433358b2
TohruHyuga <positivethinking0305@gmail.com>
2022-04-04 12:07:47 +0900

    first

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,8 +7,9 @@
     <link rel="stylesheet" href="./stylesheet.css">
     <!-- <iframe src="./founding.html" width="600" height="100"></iframe> -->
     <link href="https://fonts.googleapis.com/css?family=Caveat rel="stylesheet">
+    <meta name=”viewport” content=”width=device-width,initial-scale=1.0″>
 
-    <title>Document</title>
+    <title>愛知大学　楽園　サークル</title>
 </head>
 <body>
     <div class="header">
